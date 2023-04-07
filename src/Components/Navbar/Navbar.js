900c7869577572a2acacad651007099f0ab90644
niranjan-official <niranjansabarianth1521@gmail.com>
2023-04-07 16:50:30 +0530

    Navbar updated

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,6 +5,13 @@ function Navbar() {
   return (
     <div className='navbar'>
       <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netf;ix-logo" />
+      <div className='nav-detail'>
+        <h5 className='nav-names'>Home</h5>
+        <h5 className='nav-names'>TV Shows</h5>
+        <h5 className='nav-names'>Movies</h5>
+        <h5 className='nav-names'>My List</h5>
+      </div>
+      <img className='search' src="https://www.freepnglogos.com/uploads/search-png/search-icon-clip-art-clkerm-vector-clip-art-online-22.png" alt="search" />
       <img className='avatar' src="https://th.bing.com/th/id/OIP.CsCEiIgcxJ54PXFJ-Ep5nQHaHa?pid=ImgDet&rs=1" alt="avatar" />
     </div>
   )
